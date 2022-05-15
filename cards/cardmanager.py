27eac5479ee218c2ecbef106fc6c5d86a04078de
exceptionoff <exceptionoff@gmail.com>
2022-05-15 16:40:26 +0300

    Added files allowing to work with cards.

---
+++ cards/cardmanager.py
@@ -0,0 +1,153 @@
+# coding: utf-8
+# This file containing a class for interacting with cards using card readers
+
+import importlib
+import typing
+from cardreader_manager import CardReaderManager
+from cards.cards_list import listTypeCards
+from cards.card import Apdu_command, ApduInterface, CardInfo
+
+
+class CardManager(CardReaderManager):
+    """A class for working with different types of maps, providing a single interface.
+    """
+    _card_apdu_interface: typing.Optional[ApduInterface] = None
+    _card_info: typing.Optional[CardInfo] = None
+    _pin_is_verify: typing.Optional[bool] = None
+
+    @classmethod
+    @property
+    def card_apdu_interface(cls) -> typing.Optional[ApduInterface]:
+        if not CardManager._card_apdu_interface:
+            raise CardManager.NoSelectCardType()
+        return CardManager._card_apdu_interface
+
+    @classmethod
+    @property
+    def card_info(cls) -> typing.Optional[CardInfo]:
+        if not CardManager._card_info:
+            raise CardManager.NoSelectCardType()
+        return CardManager._card_info
+
+    @classmethod
+    @property
+    def pin_is_verify(cls) -> bool:
+        if not CardManager.connection_is_active:
+            raise CardManager.ConnectionIsNotActive()
+        if CardManager._pin_is_verify is None:
+            raise CardManager.NoSelectCardType()
+        return CardManager._pin_is_verify
+
+    @classmethod
+    def card_type_list(cls) -> typing.List[str]:
+        lst = [typeCards.typename for typeCards in listTypeCards]
+        if not lst:
+            raise CardManager.NoCardTypes()
+        return lst
+
+    @classmethod
+    def set_card_type(cls, typename: str) -> None:
+        if not (typename in cls.card_type_list()):
+            raise CardManager.NoSuchCardTypes()
+        try:
+            module = importlib.import_module('cards.' + typename)
+            cls._card_apdu_interface = module.apdu
+            cls._card_info = module.card_info
+        except ModuleNotFoundError:
+            raise CardManager.NoSuchCardTypes()
+
+    @classmethod
+    def connect(cls) -> None:
+        super().connect()
+        cls._pin_is_verify = not bool(cls._card_info.size_pin)
+
+    @classmethod
+    def disconnect(cls) -> None:
+        super().disconnect()
+        cls._pin_is_verify = None
+
+    @classmethod
+    def execute_commands(cls, commands: typing.List[Apdu_command]) -> typing.Tuple[Apdu_command, Apdu_command]:
+        if not CardManager.connection_is_active:
+            CardManager.connect()
+        sw_list = []
+        data_list = []
+        for command in commands:
+            sw, data = CardManager.transmit(command)
+            sw_list.append(sw)
+            data_list.append(data)
+        return sw_list, data_list
+
+    @classmethod
+    def select_card_type(cls) -> None:
+        sw_list, data_list = cls.execute_commands(cls._card_apdu_interface.select_card_type())
+        for sw in sw_list:
+            if sw != cls._card_info.sw_success['select_card_type']:
+                raise cls.FailureCommand()
+
+    @classmethod
+    def verify_pin(cls, pin: bytes) -> None:
+        sw_list, data_list = cls.execute_commands(cls._card_apdu_interface.verify_pin(pin))
+        for sw in sw_list:
+            if sw != cls._card_info.sw_success['verify_pin']:
+                raise cls.FailureCommand()
+        cls._pin_is_verify = True
+
+    @classmethod
+    def change_pin(cls, pin: bytes) -> None:
+        if not CardManager.pin_is_verify:
+            raise CardManager.PinIsNotVerify()
+        sw_list, data_list = cls.execute_commands(cls._card_apdu_interface.change_pin(pin))
+        for sw in sw_list:
+            if sw != cls._card_info.sw_success['change_pin']:
+                raise cls.FailureCommand()
+
+    @classmethod
+    def read_bytes(cls, offset_b: int, size_b: int) -> bytes:
+        if not CardManager.pin_is_verify and CardManager._card_info.read_need_pin:
+            raise CardManager.PinIsNotVerify()
+        sw_list, data_list = cls.execute_commands(cls._card_apdu_interface.read(offset_b, size_b))
+        for sw in sw_list:
+            if sw != cls._card_info.sw_success['read']:
+                raise cls.FailureCommand()
+        data = []
+        for data_ in data_list:
+            data += data_
+        return bytes(data)
+
+    @classmethod
+    def write_bytes(cls, offset_b: int, data: bytes) -> None:
+        if not CardManager.pin_is_verify and CardManager._card_info.write_need_pin:
+            raise CardManager.PinIsNotVerify()
+        sw_list, data_list = cls.execute_commands(cls._card_apdu_interface.write(offset_b, data))
+        for sw in sw_list:
+            if sw != cls._card_info.sw_success['write']:
+                raise cls.FailureCommand()
+
+        if cls.read_bytes(offset_b, len(data)) != data:
+            raise cls.FailureCommand()
+
+    class NoSuchCardTypes(Exception):
+        def __init__(self):
+            super().__init__()
+            self.msg = "No such card type!"
+
+    class NoCardTypes(Exception):
+        def __init__(self):
+            super().__init__()
+            self.msg = "No card types!"
+
+    class NoSelectCardType(Exception):
+        def __init__(self):
+            super().__init__()
+            self.msg = "No card type selected!"
+
+    class FailureCommand(Exception):
+        def __init__(self):
+            super().__init__()
+            self.msg = "Command failed!"
+
+    class PinIsNotVerify(Exception):
+        def __init__(self):
+            super().__init__()
+            self.msg = "PIN code not presented!"
---
+++ cards/cardreader_manager.py
@@ -0,0 +1,185 @@
+# coding: utf-8
+# This file containing a class for interacting with card readers
+from typing import List, Tuple, Optional, Any
+import logging
+
+from smartcard.scard import *
+from smartcard.System import readers
+from smartcard.util import toHexString
+
+
+logging.basicConfig(level=logging.WARNING)
+
+
+class CardReaderManager:
+    """A class for interacting with smart cards and card readers. At
+    initialization gets a list of all card readers around and does active
+    is the first card reader in the list.
+    """
+
+    class NoCardReaders(Exception):
+        def __init__(self):
+            super().__init__()
+            self.msg = "No card readers found!"
+
+    class NoSelectCardReader(Exception):
+        def __init__(self):
+            super().__init__()
+            self.msg = "No card reader selected!"
+
+    class ConnectionIsActive(Exception):
+        def __init__(self, reader: str):
+            super().__init__()
+            self.reader = str(reader)
+            self.msg = f"The connection is already established on the card reader {self.reader}!"
+
+    class ConnectionIsNotActive(Exception):
+        def __init__(self):
+            super().__init__()
+            self.msg = "The connection is not active!"
+
+    class IncorrectReaderName(Exception):
+        def __init__(self):
+            super().__init__()
+            self.msg = "Incorrect card reader name!"
+
+    class CardBackendError(Exception):
+        def __init__(self, msg: str):
+            super().__init__()
+            self.msg = msg
+
+    _current_reader: Optional[str] = None
+    _connection_is_active: bool = False
+    __hcontext: Optional[Any] = None
+    __hcard: Optional[Any] = None
+    __dwActiveProtocol: Optional[Any] = None
+
+    @classmethod
+    @property
+    def current_reader(cls) -> Optional[str]:
+        logging.info(cls._current_reader)
+        return cls._current_reader
+
+    @classmethod
+    @property
+    def connection_is_active(cls) -> bool:
+        logging.info(cls._connection_is_active)
+        return cls._connection_is_active
+
+    @classmethod
+    def readers_list(cls) -> List[str]:
+        lst = [reader.name for reader in readers()]
+        logging.info(lst)
+        return lst
+
+    @classmethod
+    def set_current_reader(cls, reader: str) -> None:
+        """Set the current card reader from which the card will be read.
+        :param reader: card reader from readers_list
+        """
+        readers_list = cls.readers_list()
+        if cls._connection_is_active:
+            raise CardReaderManager.ConnectionIsActive(cls._current_reader)
+        if not readers_list:
+            raise cls.NoCardReaders()
+        if not (reader in readers_list):
+            raise cls.IncorrectReaderName()
+        cls._current_reader = reader
+        logging.info(f"Selected card reader: {cls._current_reader}")
+
+    @classmethod
+    def connect(cls) -> None:
+        """Connect to the card in the selected card reader."""
+
+        if cls._connection_is_active:
+            raise cls.ConnectionIsActive(cls._current_reader)
+
+        if cls._current_reader is None:
+            raise cls.NoSelectCardReader()
+
+        hresult, cls.__hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
+
+        if hresult != SCARD_S_SUCCESS:
+            raise cls.CardBackendError(
+                "Ошибка SCardEstablishContext: "
+                + SCardGetErrorMessage(hresult)
+                .encode("ISO-8859-2")
+                .decode("windows-1251")
+            )
+
+        hresult, cls.__hcard, cls.__dwActiveProtocol = SCardConnect(
+            cls.__hcontext,
+            str(cls._current_reader),
+            SCARD_SHARE_SHARED,
+            SCARD_PROTOCOL_T0 | SCARD_PROTOCOL_T1,
+        )
+
+        if hresult != SCARD_S_SUCCESS:
+            raise cls.CardBackendError(
+                "Ошибка SCardConnect: "
+                + SCardGetErrorMessage(hresult)
+                .encode("ISO-8859-2")
+                .decode("windows-1251")
+            )
+
+        cls._connection_is_active = True
+        logging.info("Card connected!")
+
+    @classmethod
+    def disconnect(cls) -> None:
+        """Disconnect from the card."""
+        hresult = SCardDisconnect(cls.__hcard, SCARD_UNPOWER_CARD)
+        if hresult != SCARD_S_SUCCESS:
+            raise cls.CardBackendError(
+                "Failed to disconnect: "
+                + SCardGetErrorMessage(hresult)
+                .encode("ISO-8859-2")
+                .decode("windows-1251")
+            )
+        cls._connection_is_active = False
+        logging.info("Card disconnected!")
+
+    @classmethod
+    def get_card_ATR(cls) -> List[int]:
+        """Get the ATR of the connected card.
+        :return: ATR
+        """
+
+        if not cls._connection_is_active:
+            raise cls.ConnectionIsNotActive()
+
+        hresult, atr = SCardGetAttrib(cls.__hcard, scard.SCARD_ATTR_ATR_STRING)
+
+        if hresult != SCARD_S_SUCCESS:
+            raise cls.CardBackendError(
+                "Ошибка SCardGetAtr: "
+                + SCardGetErrorMessage(hresult)
+                .encode("ISO-8859-2")
+                .decode("windows-1251")
+            )
+
+        logging.info(f"ATR: {toHexString(atr)}")
+        return atr
+
+    @classmethod
+    def transmit(cls, apdu) -> Tuple[List[int], List[int]]:
+        """Send APDU to the map.
+        :param apdu: apdu
+        :return: SW, DATA
+        """
+        if not cls._connection_is_active:
+            raise cls.ConnectionIsNotActive()
+
+        hresult, response = SCardTransmit(cls.__hcard, cls.__dwActiveProtocol, apdu)
+        if hresult != SCARD_S_SUCCESS:
+            raise cls.CardBackendError(
+                "Ошибка transmit: "
+                + SCardGetErrorMessage(hresult)
+                .encode("ISO-8859-2")
+                .decode("windows-1251")
+            )
+        sw_ = response[-2:]
+        data_ = response[:-2]
+        logging.debug(f"> {toHexString(apdu)}")
+        logging.info(f"< {response}")
+        return sw_, data_
