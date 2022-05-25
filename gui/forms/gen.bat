b8065805444dc6afaf917bf540301042af71532d
exceptionoff <exceptionoff@gmail.com>
2022-05-25 23:55:27 +0300

    Refactor

---
+++ gui/forms/gen.bat
@@ -0,0 +1,4 @@
+python -m PyQt5.uic.pyuic -x StartWindow.ui -o StartWindow.py
+python -m PyQt5.uic.pyuic -x EnterSeedPhrase.ui -o EnterSeedPhrase.py
+python -m PyQt5.uic.pyuic -x CardConnection.ui -o CardConnection.py
+python -m PyQt5.uic.pyuic -x EncryptSeedPhrase.ui -o EncryptSeedPhrase.py
